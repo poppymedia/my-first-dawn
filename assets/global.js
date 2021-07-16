aa71cbb3a1a5b3e6501d6c89d2bf171665600d4a
Steven Henderson <556727+poppymedia@users.noreply.github.com>
2021-07-16 18:49:00 +0100

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -513,7 +513,7 @@ class VariantSelects extends HTMLElement {
       this.updateURL();
       this.updateVariantInput();
       this.renderProductInfo();
-    } if(this.currentVariant.id){} else { console.log('Unavailable: ' + this.parentElement.id); }
+    } if(this.currentVariant.id){} else { console.log('Unavailable: ' + this.parentNode.id); }
   }
 
   updateOptions() {
